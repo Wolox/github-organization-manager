c3a526dc2baa0519e0dbc7da86eea5b4e33c03eb
Sebastian Balay <snbalay@gmail.com>
2017-10-22 20:30:58 -0300

    update private repos limit

--- config/development.js
+++ config/development.js
@@ -5,7 +5,7 @@ exports.config = {
     api: {},
     github: {
       organization: process.env.NODE_MANAGER_ORGANIZATION || 'Wolox',
-      private_repositories_limit: process.env.NODE_MANAGER_PRIVATE_REPOSITORIES_LIMIT || 50
+      private_repositories_limit: process.env.NODE_MANAGER_PRIVATE_REPOSITORIES_LIMIT || 125
     },
     rollbar: {}
   }

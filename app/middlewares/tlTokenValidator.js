b54b0d870b1800b1bd93b83a101d0164307476b0
Sebastian Balay <snbalay@gmail.com>
2018-09-14 10:41:26 -0300

    validate tl token

--- app/middlewares/tlTokenValidator.js
+++ app/middlewares/tlTokenValidator.js
@@ -1,9 +1,15 @@
 const TechLeader = require('../models').techleader;
 
 exports.validateTlToken = (error, req, res, next) => {
-  TechLeader.getByToken(req.data.tlToken)
+  if (!req.data.tlToken && !req.query.tlToken) {
+    next();
+    return;
+  }
+  TechLeader.getByToken(req.data.tlToken || req.query.tlToken)
     .then(tl => {
       if (tl) {
+        req.data.token = process.env.ADMIN_TOKEN;
+        req.query.token = process.env.ADMIN_TOKEN;
         next();
       } else {
         res.status(401);
